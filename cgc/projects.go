f76ef2c494728f30c4a9a9462d597db36775cf65
Daniel Doza <doza.daniel@gmail.com>
2019-07-30 15:47:04 +0200

    implemented handing of pagination for projects

--- cgc/projects.go
+++ cgc/projects.go
@@ -18,20 +18,34 @@ func (c Client) Projects() ([]Project, error) {
 	u := mustParseURL(c.baseURL)
 	u.Path += "projects"
 
-	resp, err := c.request(http.MethodGet, u, nil)
-	if err != nil {
-		return nil, fmt.Errorf("fetching files failed: %s", err.Error())
-	}
-	defer resp.Close()
+	projects := make([]Project, 0)
 
-	// TODO: paging
-	var r struct {
-		apiOKResponseTemplate
-		Items []Project `json:"items"`
-	}
-	if err := json.NewDecoder(resp).Decode(&r); err != nil {
-		return nil, fmt.Errorf("unmarshalling response failed: %s", err.Error())
+	// if there's more projects than returned by default by the API, links array will
+	// be provided. The object that has the 'rel' field with the value of 'next' will
+	// also contain the 'href' with the complete link to the next page.
+	for u != nil {
+		resp, err := c.request(http.MethodGet, u, nil)
+		if err != nil {
+			return nil, fmt.Errorf("fetching files failed: %s", err.Error())
+		}
+		defer resp.Close()
+
+		var r struct {
+			apiOKResponseTemplate
+			Projects []Project `json:"items"`
+		}
+		if err := json.NewDecoder(resp).Decode(&r); err != nil {
+			return nil, fmt.Errorf("unmarshalling response failed: %s", err.Error())
+		}
+		projects = append(projects, r.Projects...)
+
+		u = nil
+		for _, link := range r.Links {
+			if link.Rel == "next" {
+				u = mustParseURL(link.Href)
+			}
+		}
 	}
 
-	return r.Items, nil
+	return projects, nil
 }
